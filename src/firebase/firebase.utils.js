4d303355e1fb4bc43e15b36734fadab5caa88903
mgplanas <mgplanas@gmail.com>
2021-08-31 09:00:18 -0300

    add redux, user reducer, set current user, connect header and app

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,40 +3,39 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+    apiKey: "AIzaSyDWi08gBu95QThZyktsjNXKtSTXbmZ2eAI",
+    authDomain: "crown-db-ba6c1.firebaseapp.com",
+    projectId: "crown-db-ba6c1",
+    storageBucket: "crown-db-ba6c1.appspot.com",
+    messagingSenderId: "430868118114",
+    appId: "1:430868118114:web:ab18fe541badfc9df3ef3b"
 };
 
 firebase.initializeApp(config);
 
-export const createUserProfileDocument = async (userAuth, additionalData) => {
-  if (!userAuth) return;
+export const createUserProfileDocument = async(userAuth, additionalData) => {
+    if (!userAuth) return;
 
-  const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
 
-  const snapShot = await userRef.get();
+    const snapShot = await userRef.get();
 
-  if (!snapShot.exists) {
-    const { displayName, email } = userAuth;
-    const createdAt = new Date();
-    try {
-      await userRef.set({
-        displayName,
-        email,
-        createdAt,
-        ...additionalData
-      });
-    } catch (error) {
-      console.log('error creating user', error.message);
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            });
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
     }
-  }
 
-  return userRef;
+    return userRef;
 };
 
 export const auth = firebase.auth();
